30408ab07e2a2a883e8fb4d0610930bdd9c547ef
D. Scott Boggs <scott@tams.tech>
2018-10-14 20:51:36 -0400

    rewrote session to be smarter about token expiration

--- session.go
+++ session.go
@@ -1,100 +1,162 @@
 package auth
 
 import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"log"
+	"math/big"
 	"time"
-
-	random "github.com/dscottboggs/go-random-string"
 )
 
-const sessionKeyLength = 1 << 7
+const (
+	// 128 bits
+	sessionKeyLength = 1 << 7
+	// one week in seconds
+	oneWeek = time.Second * 86400 * 7
+	// the maximum number of bits that can fit in an uint8
+	byteSizeConst = 1<<8 - 1
+)
 
-var expiryDelay = 24 * 30 * time.Hour
+var (
+	// the default amount of time until a token expires: 30 days
+	expiryDelay = 24 * 30 * time.Hour
+	// a big.Int representing the maximum number that can fit in an uint8
+	byteSize = big.NewInt(byteSizeConst)
+	// AllSessions stores each valid token
+	AllSessions map[Session]*SessionMetadata
+	// nullSession is found when the session doesn't exist
+	nullSession Session
+	// how frequently to sweep for expired tokens
+	sweepDelay   time.Duration = 10 * time.Second
+	sweeper                    = time.NewTicker(sweepDelay)
+	sweepQuitter               = make(chan bool)
+)
 
 // The Session cookie store
 type Session [sessionKeyLength]byte
 
-// AllSessions stores each valid token
-var AllSessions []Session
-
-var nullSession Session
+type SessionMetadata struct {
+	Expiry time.Time
+}
 
 func init() {
-	AllSessions = make([]Session, 0)
+	AllSessions = make(map[Session]*SessionMetadata)
+	go sweep()
 }
 
-// HasSession returns true if the given token was found in AllSessions
-func HasSession(token string) bool {
-	if len(token) != sessionKeyLength {
-		return false
-	}
-	var t Session
-	copy(t[:], []byte(token))
-	return t.CurrentlyExists()
-}
-
-// CurrentlyExists --
-// Checks that the given session token is present in the list of permitted
-// sessions.
-func (s *Session) CurrentlyExists() bool {
+// GetSession finds the token in AllSessions and returns the metadata
+func (s *Session) GetMetadata() (sesh *SessionMetadata, found bool) {
 	if (*s) == nullSession {
-		return false
+		return
 	}
-	for _, session := range AllSessions {
-		if session == (*s) {
-			return true
-		}
+	sesh = AllSessions[*s]
+	if sesh != nil && sesh.Expiry.Unix() > 0 {
+		found = true
 	}
-	return false
+	return
 }
 
-// Expire the session after the default period.
-func (s *Session) Expire() {
-	s.ExpireIn(expiryDelay)
-}
-
-// ExpireIn the given delay.
-func (s *Session) ExpireIn(delay time.Duration) {
-	go func() {
-		time.Sleep(delay)
-		s.Delete()
-	}()
+func (s *Session) CurrentlyExists() (found bool) {
+	_, found = s.GetMetadata()
+	return
 }
 
 // Delete the given token from the list of allowed sessions.
 func (s *Session) Delete() {
-	for index, token := range AllSessions {
-		if (*s) == token {
-			AllSessions = append(AllSessions[:index], AllSessions[index+1:]...)
+	delete(AllSessions, *s)
+}
+
+func (s *Session) ExpireIn(duration time.Duration) error {
+	sesh, ok := s.GetMetadata()
+	if !ok {
+		return fmt.Errorf("Session not found")
+	}
+	sesh.Expiry = time.Now().Add(duration)
+	return nil
+}
+
+func (s *Session) ExpireAt(t time.Time) error {
+	sesh, ok := s.GetMetadata()
+	if !ok {
+		return fmt.Errorf("Session not found")
+	}
+	sesh.Expiry = t
+	return nil
+}
+
+func SetDefaultExpiry(t time.Duration) {
+	expiryDelay = t
+}
+
+// NewSession returns a new random token.
+func NewSession() (Session, *SessionMetadata) {
+	var (
+		token    Session
+		temp     *big.Int
+		err      error
+		metadata = &SessionMetadata{
+			Expiry: time.Now().Add(expiryDelay),
+		}
+	)
+	for i := 0; i < sessionKeyLength; i++ {
+		temp, err = rand.Int(rand.Reader, byteSize)
+		if err != nil {
+			log.Fatalf("error reading from random number generator! %v", err)
+		}
+		token[i] = byte(temp.Int64())
+	}
+	if token.CurrentlyExists() {
+		token, metadata = NewSession()
+	}
+	AllSessions[token] = metadata
+	return token, metadata
+}
+
+// SetCleanupInterval sets how frequently to sweep for expired tokens
+func SetCleanupInterval(interval time.Duration) {
+	sweeper.Stop()
+	sweepQuitter <- true
+	sweepDelay = interval
+	sweeper = time.NewTicker(interval)
+	go sweep()
+}
+
+// sweep the AllSessions object and clean up any expired sessions
+func sweep() {
+	for {
+		select {
+		case <-sweepQuitter:
+			return
+		case <-sweeper.C:
+			doSweep()
 		}
 	}
 }
 
-// Delete the given token, given as a string.
-func Delete(token string) {
-	var ts Session
-	copy(ts[:], []byte(token))
-	ts.Delete()
+func doSweep() {
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		sweepDelay,
+	)
+	defer cancel()
+	for sesh, metadata := range AllSessions {
+		select {
+		case <-ctx.Done():
+			incrementSleepDelay()
+			log.Printf(
+				"WARNING took too long to sweep expired settings, raising "+
+					"delay to %d seconds.",
+				sweepDelay/time.Second,
+			)
+		default:
+			if metadata.Expiry.Unix() < time.Now().Unix() {
+				sesh.Delete()
+			}
+		}
+	}
 }
 
-// SetExpiryDelay changes the delay for session expiry from the default of 2 hours
-func SetExpiryDelay(dTime time.Duration) {
-	expiryDelay = dTime
-}
-
-// NewSession returns a new random token.
-func NewSession() string {
-	s := newSession()
-	return string(s[:])
-}
-func newSession() Session {
-	var token Session
-	for i := 0; i < sessionKeyLength; i++ {
-		token[i] = byte(random.Alphanumeric())
-	}
-	if token.CurrentlyExists() {
-		token = newSession()
-	}
-	AllSessions = append(AllSessions, token)
-	token.Expire()
-	return token
+func incrementSleepDelay() {
+	sweepDelay = sweepDelay * 15 / 10
 }
--- session_test.go
+++ session_test.go
@@ -8,34 +8,27 @@ import (
 	"github.com/dscottboggs/attest"
 )
 
-func TestHasSession(t *testing.T) {
-	test := attest.New(t)
-	token := NewSession()
-	test.Logf("Token: %#+\n", token)
-	if !HasSession(token) {
-		test.Error("does not have session from NewToken")
-	}
-}
 func BenchmarkNewSession(b *testing.B) {
 	for index := 0; index < b.N; index++ {
-		_ = newSession()
+		_, _ = NewSession()
 	}
 }
 func BenchmarkHasSession(bench *testing.B) {
 	// setup some sessions
 	var (
-		token string
+		token Session
 		round uint
 	)
 	for round = 1; round < 8; round++ {
 		bench.Run(fmt.Sprintf("with %d users", 1<<round), func(b *testing.B) {
 			for index := 0; index < 1<<round; index++ {
-				token = NewSession()
+				// create variously sized maps to search
+				token, _ = NewSession()
 			}
 			// reset the timer
 			b.ResetTimer()
 			for index := 0; index < b.N; index++ {
-				if !HasSession(token) {
+				if !token.CurrentlyExists() {
 					b.Fail()
 				}
 			}
@@ -43,38 +36,92 @@ func BenchmarkHasSession(bench *testing.B) {
 	}
 }
 
-func TestExpiryAndDeletion(t *testing.T) {
-	test := attest.NewTest(t)
-	token := newSession()
+func TestExpiry(t *testing.T) {
+	SetCleanupInterval(500 * time.Millisecond)
+	prechecks := func(
+		test *attest.Test, token Session, metadata *SessionMetadata,
+	) {
+		test.DiffersByLessThan(
+			int64(2),
+			time.Now().Add(expiryDelay).Unix(),
+			metadata.Expiry.Unix(),
+		)
+		if !token.CurrentlyExists() {
+			test.Fatal("newly created session didn't exist")
+		}
+	}
+	postchecks := func(
+		test *attest.Test, token Session, metadata *SessionMetadata,
+	) {
+		if token.CurrentlyExists() {
+			test.Error("token still existed after expiry time")
+		}
+		time.Sleep(1 * time.Second)
+		if token.CurrentlyExists() {
+			test.Error("token still existed another second later")
+		}
+		test.LessThan(time.Now().Unix(), metadata.Expiry.Unix())
+	}
+	t.Run("ExpireIn()", func(t *testing.T) {
+		t.Run("valid session", func(t *testing.T) {
+
+			test := attest.NewTest(t)
+			token, metadata := NewSession()
+			prechecks(&test, token, metadata)
+			token.ExpireIn(1 * time.Second)
+			time.Sleep(2500 * time.Millisecond)
+			postchecks(&test, token, metadata)
+		})
+		t.Run("without valid session", func(t *testing.T) {
+			test := attest.New(t)
+			token, _ := NewSession()
+			token.Delete()
+			if token.CurrentlyExists() {
+				t.Error("token existed after deletion")
+			}
+			test.NotNil(
+				token.ExpireIn(1*time.Second),
+				"got nil error from ExpireIn() on deleted token",
+			)
+		})
+	})
+	t.Run("ExpireAt()", func(t *testing.T) {
+		t.Run("with valid session", func(t *testing.T) {
+
+			test := attest.NewTest(t)
+			token, metadata := NewSession()
+			prechecks(&test, token, metadata)
+			token.ExpireAt(time.Now().Add(1 * time.Second))
+			time.Sleep(2500 * time.Millisecond)
+			postchecks(&test, token, metadata)
+		})
+		t.Run("without valid session", func(t *testing.T) {
+			test := attest.New(t)
+			token, _ := NewSession()
+			token.Delete()
+			if token.CurrentlyExists() {
+				t.Error("token existed after deletion")
+			}
+			test.NotNil(
+				token.ExpireAt(time.Now().Add(1*time.Second)),
+				"got nil error from ExpireIn() on deleted token",
+			)
+		})
+	})
+}
+func TestDeletion(t *testing.T) {
+	test := attest.New(t)
+	token, metadata := NewSession()
+	test.DiffersByLessThan(
+		int64(2),
+		time.Now().Add(expiryDelay).Unix(),
+		metadata.Expiry.Unix(),
+	)
 	if !token.CurrentlyExists() {
-		test.Fatal("newly created session didn't exist")
-	}
-	token.ExpireIn(2 * time.Second)
-	time.Sleep(2500 * time.Millisecond)
-	if token.CurrentlyExists() {
-		test.Error("token still existed after expiry time")
-	}
-	time.Sleep(1 * time.Second)
-	if token.CurrentlyExists() {
-		test.Error("token still existed another second later")
-	}
-	token = newSession()
-	if !token.CurrentlyExists() {
-		test.Fatal("newly created session didn't exist")
+		t.Errorf("token doesn't exist")
 	}
 	token.Delete()
 	if token.CurrentlyExists() {
-		test.Error("token still exists after deletion")
-	}
-	tokenString := NewSession()
-	if !HasSession(tokenString) {
-		test.Fatal("newly created session didn't exist")
-	}
-	Delete(tokenString)
-	if HasSession(tokenString) {
-		test.Error("token still exists after deletion")
-	}
-	if nullSession.CurrentlyExists() {
-		test.Error("null session is authenticated!")
+		t.Errorf("token exists")
 	}
 }
